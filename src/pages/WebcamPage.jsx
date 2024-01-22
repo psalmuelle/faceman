df750d3a088f046c781e8488ddc717c76b88ac52
psalmuelle <bjoestores@gmail.com>
2024-01-22 13:06:44 +0100

    chore: implement tensorflow

--- src/pages/WebcamPage.jsx
+++ src/pages/WebcamPage.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Webcam from "react-webcam";
 
+import "@tensorflow/tfjs";
+import * as faceDetection from "@tensorflow-models/face-detection";
 
 export default function WebcamPage() {
   const videoConstraints = {
-    width: 200,
-    height: 230,
+    width: 400,
+    height: 460,
     facingMode: "user",
+    aspectRatio: 0.1,
   };
-
-  
 
   const [error, setError] = React.useState(false);
   const [images, setImages] = React.useState([]);
@@ -18,12 +19,33 @@ export default function WebcamPage() {
 
   const webcamRef = React.useRef(null);
 
-  React.useEffect(() => {
+  React.useEffect(() => {}, []);
 
+  const handlePlay = async () => {
+    const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
 
-  }, []);
+    const detectorConfig = {
+      runtime: "tfjs",
+    };
 
+    const detector = await faceDetection.createDetector(model, detectorConfig);
 
+    setInterval(async () => {
+      if (
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null
+      ) {
+        const faces = await detector.estimateFaces(webcamRef.current.video);
+
+        if (faces[0]) {
+          setImages((prev) => [...prev, webcamRef.current.getScreenshot()]);
+          setIsOkay(true);
+        } else {
+          setIsOkay(false);
+        }
+      }
+    }, 1000);
+  };
 
   const handleCaptureClick = () => {
     const checkUserMediaSupport = async () => {
@@ -43,15 +65,16 @@ export default function WebcamPage() {
       }
     };
 
-    checkUserMediaSupport().then(()=>{
-      if (startCapture) {
-        setStartCapture(false);
-      } else {
-        setImages([]);
-        !error && setStartCapture(true);
-      }
-    }).catch(err=> console.log(err))
-
+    checkUserMediaSupport()
+      .then(() => {
+        if (startCapture) {
+          setStartCapture(false);
+        } else {
+          setImages([]);
+          !error && setStartCapture(true);
+        }
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -69,6 +92,8 @@ export default function WebcamPage() {
             screenshotFormat='image/jpeg'
             videoConstraints={videoConstraints}
             screenshotQuality={1}
+            onPlay={handlePlay}
+            mirrored={true}
           />
         </div>
       ) : (
